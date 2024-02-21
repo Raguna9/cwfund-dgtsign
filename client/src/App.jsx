2083be83987500889899e05789d829494eaeba28
Raguna9 <dickyuzumaki73@gmail.com>
2024-02-21 21:28:29 +0800

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-black'>Hello World!!!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
