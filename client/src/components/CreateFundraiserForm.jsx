dac884be732239a3d8a5fa617fa71b090bc461e0
Raguna9 <dickyuzumaki73@gmail.com>
2024-03-04 02:05:44 +0800

    fix: create fundraiser now connect with contract

--- client/src/components/CreateFundraiserForm.jsx
+++ client/src/components/CreateFundraiserForm.jsx
@@ -1,9 +1,93 @@
 import React, { useState, useCallback, useEffect } from "react";
 import { useWeb3 } from "./Web3Provider";
-// import Notification from "../Notification";
+import Notification from "./Notification";
 
 const CreateFundraiserForm = () => {
-  const { accounts } = useWeb3();
+  const { accounts, factory } = useWeb3();
+  const [formState, setFormState] = useState({
+    tittle: '',
+    description: '',
+    url: '',
+    imageURL: '',
+    beneficiary: '',
+  });
+  const [notificationOpen, setNotificationOpen] = useState(false);
+  const [successMsg, setSuccessMsg] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleInputChange = useCallback((e) => {
+    const { name, value } = e.target;
+    setFormState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }, []);
+
+  const handleCreateFundraiser = useCallback(async () => {
+    setLoading(true);
+    try {
+      const createFundraiserFunc = await factory.methods
+        .createFundraiser(formState.tittle, formState.description, formState.url, formState.imageURL, formState.beneficiary)
+        .send({ from: accounts[0] });
+      console.log(createFundraiserFunc);
+      // if (createFundraiserFunc) {
+      setNotificationOpen(true);
+      setSuccessMsg(`Fundraiser successfully created`);
+      resetForm();
+      // }
+    } catch (err) {
+      console.error(err);
+      setNotificationOpen(true);
+      if (!formState.tittle || !formState.description || !formState.url || !formState.imageURL || !formState.beneficiary) {
+        setErrorMsg("Input field must not be empty!")
+      }
+      else {
+        if (err.code == '4001') {
+          setErrorMsg(err.message);
+          console.log(err);
+        } if (err.code == '-32603') {
+          setErrorMsg(err.message);
+          console.log(err);
+        } if (err.code == 'INVALID_ARGUMENT') {
+          setErrorMsg(`Invalid ${err.argument} : ${err.value} (${err.code})`);
+          console.log(err);
+        }
+        else {
+          setErrorMsg(err);
+        }
+      }
+    }
+  }, [accounts, factory, formState]);
+
+  const resetForm = useCallback(() => {
+    setFormState({
+      tittle: '',
+      description: '',
+      url: '',
+      imageURL: '',
+      beneficiary: '',
+    });
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    resetForm();
+    setNotificationOpen(false);
+    setErrorMsg('');
+    setSuccessMsg('');
+  }, [accounts]);
+
+  useEffect(() => {
+    setLoading(false);
+    const timer = setTimeout(() => {
+      setNotificationOpen(false);
+      setErrorMsg('');
+      setSuccessMsg('');
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [notificationOpen, errorMsg]);
 
   return (
     <>
@@ -21,61 +105,72 @@ const CreateFundraiserForm = () => {
           />
         </div>
         <div className="mb-4">
-          <label className="block text-gray-700 font-medium mb-2">Name</label>
+          <label className="block text-gray-700 font-medium mb-2">Tittle</label>
           <input
             type="input"
-            name="name"
+            name="tittle"
             placeholder="ex. John Doe"
             className="block w-full border border-gray-400 px-4 py-2 rounded focus:outline-none focus:border-gray-500"
-          // value={formState.name}
-          // onChange={handleInputChange}
+            value={formState.tittle}
+            onChange={handleInputChange}
           />
         </div>
         <div className="mb-2">
-          <label className="block text-gray-700 font-medium mb-2">Role</label>
-        </div>
-        <div className="inline-block relative w-full mb-4">
-          <select
-            className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-3 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline"
-            name="role"
-          // value={formState.role}
-          // onChange={handleInputChange}
-          >
-            {/* <option value="1">{rolesName[1]}</option>
-                        <option value="2">{rolesName[2]}</option>
-                        <option value="3">{rolesName[3]}</option>
-                        <option value="4">{rolesName[4]}</option> */}
-          </select>
-          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3 pb-2 text-gray-700">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" clipRule="evenodd" d="M10 13.415l5.707-5.707a1 1 0 111.414 1.414l-6.364 6.364a.997.997 0 01-1.414 0L2.879 8.122a1 1 0 011.414-1.414L10 13.415z" />
-            </svg>
-          </div>
+          <label className="block text-gray-700 font-medium mb-2">Description</label>
+          <textarea
+            type="input"
+            name="description"
+            placeholder="ex. Raising funds for orphanage children"
+            className="block w-full border border-gray-400 px-4 py-2 rounded focus:outline-none focus:border-gray-500"
+            value={formState.description}
+            onChange={handleInputChange}
+          />
         </div>
         <div className="mb-4">
-          <label className="block text-gray-700 font-medium mb-2">User Address</label>
+          <label className="block text-gray-700 font-medium mb-2">URL</label>
           <input
             type="input"
-            name="place"
-            placeholder="ex. Mataram"
+            name="url"
+            placeholder="ex. www.kitabisa.com"
             className="block w-full border border-gray-400 px-4 py-2 rounded focus:outline-none focus:border-gray-500"
-          // value={formState.place}
-          // onChange={handleInputChange} 
+            value={formState.url}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-medium mb-2">Image URL</label>
+          <input
+            type="input"
+            name="imageURL"
+            placeholder="ex. www.kitabisa.com"
+            className="block w-full border border-gray-400 px-4 py-2 rounded focus:outline-none focus:border-gray-500"
+            value={formState.imageURL}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-medium mb-2">Beneficiary</label>
+          <input
+            type="input"
+            name="beneficiary"
+            placeholder="ex. 0x000000000000000000000000000000000"
+            className="block w-full border border-gray-400 px-4 py-2 rounded focus:outline-none focus:border-gray-500"
+            value={formState.beneficiary}
+            onChange={handleInputChange}
           />
         </div>
         <div className="text-center">
           <button
-            // disabled={loading}
+            disabled={loading}
             className="bg-blue-500 w-full text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-200"
-          // onClick={handleRegisterActor}
+            onClick={handleCreateFundraiser}
           >
-            {/* {loading ? "Loading..." : "Register"} */}
-            Register
+            {loading ? "Loading..." : "Register"}
           </button>
         </div>
       </div>
-      {/* {notificationOpen && successMsg != '' && <Notification msg={successMsg} open={notificationOpen} bgColor="green" />}
-      {notificationOpen && errorMsg != '' && <Notification msg={errorMsg} open={notificationOpen} bgColor="red" />} */}
+      {notificationOpen && successMsg != '' && <Notification msg={successMsg} open={notificationOpen} bgColor="green" />}
+      {notificationOpen && errorMsg != '' && <Notification msg={errorMsg} open={notificationOpen} bgColor="red" />}
     </>
   );
 };
