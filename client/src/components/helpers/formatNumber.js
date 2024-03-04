1451b9606b23dace61bfa2e64d1e792077990ba1
Raguna9 <dickyuzumaki73@gmail.com>
2024-03-04 23:29:45 +0800

    add: formatNumber for IDR

---
+++ client/src/components/helpers/formatNumber.js
@@ -0,0 +1,14 @@
+/**
+ * Memformat bilangan bulat tertentu menjadi angka berbasis IDR
+ * @param {number} num menampung bilangan yang belum diformat untuk diformat
+ * @returns {string} men-format angka ke format IDR dan 2 angka setelah koma
+ */
+const formatNumber = num => {
+    if (!num) return ''
+    const formatted = num.toLocaleString('id-ID', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    })
+    return formatted
+}
+export default formatNumber
